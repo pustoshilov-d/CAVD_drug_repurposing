1c1c946af97e573bcded30e47f32efcda732df91
Dmitry Pustoshilov <pustoshilov.d@gmail.com>
2022-07-23 13:09:21 +0300

    savee

--- parseDatabases.js
+++ parseDatabases.js
@@ -75,6 +75,7 @@ const getBioEnity = async(id) => {
             action: row.querySelector('td:nth-of-type(5)')?.textContent || ""
         }
     ))
+    console.log(id, name)
     
 
     return {
@@ -574,19 +575,54 @@ const runDrugBankPharmaco = (genes) => {
 
 const runDrugBankBEProteinsOnly = async(entityList) => {
     const infoList = await Promise.all(entityList.map(async entity => {
-        console.log(entity)
+        // console.log(entity)
         if (!entity) return {}
         const bioEnity = await getBioEnity(entity)
         // console.log(bioEnity)
 
         return {
             id: bioEnity.id,
-            proteinsNames: bioEnity.proteins.map(i => i?.name || '').join(';!'),
-            proteinsID: bioEnity.proteins.map(i => i?.id || '').join(';!')
+            name: bioEnity.name,
+            // proteinsNames: bioEnity.proteins.map(i => i?.name || '').join(';!'),
+            // proteinsID: bioEnity.proteins.map(i => i?.id || '').join(';!')
         }
     }))
 
     saveToTSVWithFlat(infoList, 'BEProteinsOnly.tsv')
+}
+
+const getCID = async(smiles) =>{
+    console.log(smiles)
+    let url = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/smiles/${smiles}/JSON`
+    console.log(url)
+
+    const res = await fetchAsync(url)
+
+    // const document = parse(res)
+    let cid
+    try {
+        cid = JSON.parse(res)["PC_Compounds"][0]['id']['id']['cid'] || ''
+    } catch {
+        cid = ''
+    }
+    
+
+    console.log(cid)
+    return {
+        smiles,
+        cid
+    }
+}
+
+const findCIDbySmiles = async(smilesList) => {
+    let infoList = []
+    console.log(smilesList)
+    for (let smiles of smilesList) {
+        console.log(smiles)
+        if (!smiles) infoList.push({})
+        else infoList.push(await getCID(smiles))
+    }
+    saveToTSVWithFlat(infoList, 'cids.tsv')
 }
 
 const runDrugBankEntities = async(entityList) => {
@@ -1201,90 +1237,99 @@ const downloadAlphaFoldPDBs = async (rowsCur) => {
 
 
 
-// let articles = ""
-// let articleList = articles.split(",")
-// runDrugBankArticles(articleList)
+let articles = ""
+let articleList = articles.split(",")
+runDrugBankArticles(articleList)
 
-// let entities = "BE0000030,BE0000147,BE0000211,BE0000215,BE0000264,BE0000270,BE0000393,BE0000393,BE0000394,BE0000428,BE0000428,BE0000428,BE0000457,BE0000457,BE0000635,BE0000704,BE0000704,BE0000838,BE0000854,BE0000854,BE0000854,BE0000854,BE0000877,BE0000918,BE0000918,BE0000918,BE0000922,BE0000944,BE0000956,BE0000961,BE0000979,BE0000981,BE0000982,BE0000984,BE0000984,BE0000985,BE0000985,BE0000985,BE0001046,BE0001047,BE0001047,BE0001051,BE0001051,BE0001060,BE0001062,BE0001062,BE0001096,BE0001097,BE0001097,BE0001114,BE0001164,BE0001172,BE0001172,BE0001180,BE0001283,BE0001283,BE0001283,BE0001353,BE0001353,BE0001646,BE0001646,BE0002204,BE0002401,BE0002401,BE0002408,BE0002408,BE0002409,BE0002409,BE0002442,BE0002442,BE0002442,BE0002683,BE0003309,BE0003309,BE0003418,BE0003428,BE0003428,BE0003433,BE0003449,BE0003450,BE0003461,BE0003462,BE0003462,BE0003481,BE0003493,BE0003493,BE0003517,BE0003517,BE0003517,BE0003546,BE0003546,BE0003546,BE0003546,BE0003546,BE0003546,BE0003640,BE0003722,BE0003768,BE0003772,BE0003772,BE0003790,BE0003790,BE0003790,BE0003997,BE0004145,BE0004145,BE0004506,BE0004506,BE0004578,BE0004583,BE0004741,BE0004807,BE0004903,BE0004903,BE0004903,BE0004903,BE0004907,BE0005055,BE0005076,BE0005551,BE0005803,BE0005813,BE0008643,BE0008643,BE0008684,BE0008694,BE0008716,BE0009000,BE0009048,BE0009143,BE0009272,BE0009324,BE0009345,BE0009415,BE0009550,BE0009668,BE0009698,BE0009698,BE0009859,BE0009865,BE0009900,BE0009903,BE0010032,BE0010032"
-// entities = "BE0000985"
-// let entityList = entities.split(",")
-// runDrugBankEntities(entityList)
+let entities = "BE0000030"
+entities = "BE0000985"
+entityList = entities.split(",")
+runDrugBankEntities(entityList)
 
-// let drugList = ""
-// drugList = fs.readFileSync('ids.txt').toString()
-// drugList = drugList.split('\n').slice(0,-1).map(i => i.replace('\r',''))
-// drugList = "DB05767"
-// drugList = drugList.split(",")
-// runDrugBankDrugs(drugList)
+let drugList = ""
+drugList = fs.readFileSync('ids.txt').toString()
+drugList = drugList.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+drugList = "DB05767"
+drugList = drugList.split(",")
+runDrugBankDrugs(drugList)
 
-// let whats = ['transcriptomics', 'proteomics'] //proteomics / transcriptomics
-// let change = 'increased' // decreased / increased
-// let genes =  "TYK2,JAK1,JAK2,JAK3,FLT3,FLT4,PTGS2,MAPK2,MAPK1,PDIA3,FBN1,ADAMTSL4,BSG,XDH,TLR4,RELA,HMGCR,NLRP3,NOX2,CYBB,PRKAA1,PRKAA2,PRKAB1,PRKAB3,PRKAG1,PRKAG2,PRKAG3,NFKB2,NFKB1,AKT1,AKT2,AKT3,BMP2,BMP4,BMP6,TGFB1,TGFB3,IL1B,CHUK,IKBKB,IKBKG,ACVR1,BMPR1B,ALK2,ALK3,SMAD1,SMAD5,SMAD8,ACVR1,BMPR1A,PPARG,VDR,ABCC5,CBR1,GSTP1,PSENEN,APH1A,APH1B,NCSTN,PSEN1,CTNNB1,SLC39A13,SLC39A14"
-// genes = genes.split(',')
-// runDrugBankPharmaco(genes)
-
-
-// let cids = "564,2141,5362485,443130,1054,53235510,5284616,,,10341154,,5318517,10288191,,,10297982,6918055,,,,62662,,5526,,8761,,,,,,,,3447,23978,23672064,24456,,,,,1014,4369359,447256,448436,6327350,2734054,6918537,,11253490,,,11450633,11243969,60823,205131,5494385,11840904,16122596,60206636,24768547,24752844,15953860,23645698,11538992,23661697,11554165,11495704,10450114,24832041,11710963,11572962,10024590,11840913,11840917,,11235729,10198228,16750059,5494422,,24798721,5459374,11974440,,16058650,11516136,134497,42608447,24180585,10320144,4747355,23647311,15991577,6914643,10096344,,,,938,3720,2812,2662,5957,261004,107758,,,,24798741,24798742,5280961,445154,5460341,,,,,,12041,,8515,6540289,16058637,6852206,9543521,25113171,2733526,1548887,448991,160355,97297,,,54675783,,2585,5281034,2244,46937095,9918381,11167602,11671467,4740,6253,6912404,6992140,,,5288266,11005,5284596,,,2895,17753880,,,,,440473,28417,,446320,84704,,,,2783,4887,3698,,,,,9871074,,,,,,107778,,134780,,5816,7028,,22524140,,13136,5573,4008,656668,15558638,456468,,5576,3291,5734,60663,941361,2520,2761,6476,644019,,,5833,2482,,3226,8280,2157,119171,4567,2162,3333,3784,4507,2351,6324659,6228,31260,446127,60594,5287974,444570,444569,,6914625,6914626,6914627,20843156,6914629,6914633,5288557,11963546,444567,5493444,4201,5362119,,,3652,2442,,,,5401,,23994,,,,5568,4199,3337,5362436,15942713,5276854,11954293,,47811,31101,28864,54746,6005,6540428,10531,2801,667468,1615,8969,2170,4184,8223,8226,,4830,27400,,43815,60795,60857,2913,11154555,5666,5287799,23648296,5287870,11840930,11840933,11840934,6420159,46937102,9547943,46937163,9887557,46937176,5467,5205,,,,49803313,667466,2726,9677,3821,3085218,216325,702,,31275,,,5757,5991,441298,446313,14985,5381226,6436173,447912,176870,6451164,36314,148124,5881,,2554,5342,,1775,392622,3292,4060,8271,4737,202225,449459,5743,3054,727,55245,8955,5564,10133,3198,4189,5353853,3823,2907,3397,3690,39042,2802,3339,54678486,192197,4764,40326,,638024,5280343,9818306,222757,,,13791,64929,,,9977819,236702,,24462,,,216326,9915886,,10257882,,,,3715,,,13342,452548,30323,,457193,3062316,446795,5287515,30819,867,,447522,,5287555,,12793,447523,,971,311,447518,5287544,4369496,,,,,,5287461,447536,5287463,,5287488,447532,5287550,75791,24905143,4708,,16040294,447534,,24905153,24905152,46937131,448171,5289215,10401956,5328969,5328940,,,643975,5162682,2094,4644,,1103,4055,,1046,,5566,,667490,2265,5288320,449035,,,38904,7697,6021,,,27661,,60961,134018,5284627,6083,4091,,,,,,,,,,,9458,637568,,,,,446156,,,,131704231,57525501,1662,439196,,,,64715,,,,,5494425,25126798,,,44205240,,,,,,,,,24963047,,,,,,,,9935073,,,,,61635,,216239,5329102,,,,3038522,,,9829523,68165256,,9911830,,,,,70788951,,,,92337,16741,,11175137,,10172943,,,6918562,,5310967,,,,5326727,44462678,16750106,10134160,766948,16129582,,,45270264,44224261,24801862,,,25203945,,,16122612,,10680,,,,,,,,,,9864881,16145453,,12082259,9888484,,,208901,,65981,,3478,65999,,,6443013,,24875301,,46937060,,,,,181137,11395145,,644213,,6006216,,23654841,2742752,,11460087,3672,10274777,39912,,,9826451,2787,,969516,5280934,,3121,2969,637517,,,,,,,,,8759,,,67678,,,22128,,,,,,,440995,,,,,10976469,6096886,16115004,,11493598,,4592,,,5164,,8064,7478,,,133021,,,5321765,802,,,,23640750,,12374,,129148,,,,221493,131505,46937097,2267,,,,,,,,3581192,10130120,,,46936357,,,,,,5360696,,,,"
-// cids = fs.readFileSync('ids.txt').toString()
-// cids = cids.split('\n').slice(0,-1).map(i => i.replace('\r',''))
-// console.log(cids.length)
-// cids = "129631922"
-// cids = cids.split(',')
-// runPubchemMol(cids)
-
-// entities = "BE0001283,,BE0003546,BE0000918,,,BE0009859,BE0000211,BE0005055,BE0000877,,BE0000854,,BE0000635,,BE0009698,BE0000428,BE0005813,,BE0000457,,BE0008643,,,BE0003428,,BE0003790,,BE0001062,,BE0000979,,BE0002409,,,BE0004741,,,,BE0001096,BE0002442,BE0001164,,BE0001047,BE0003418,,BE0000704,,BE0008716,,BE0003309,BE0000270,BE0010032,BE0000982,,,BE0001046,BE0000984,BE0009048,,BE0000393,,,,,,,,,,BE0001646,,,BE0001114,,BE0004506,BE0003462,BE0000956,BE0009143,BE0008684,,BE0003517,BE0009345,BE0001060,,,BE0001180,BE0003640,,BE0003517,BE0000838,,,BE0003461,BE0000457,,,BE0004907,BE0004903,BE0001172,BE0000428,,,BE0005803,BE0003546,BE0009550,,BE0000985,,BE0000985,,,,,,,,,BE0000918,BE0000961,BE0001051,BE0001047,,BE0009900,BE0009903,BE0003546,BE0005076,BE0004145,BE0002408,BE0003493,,BE0000147,BE0000264,BE0009272,BE0009415,BE0009324,BE0004903,,,BE0001051,BE0002442,BE0004903,BE0008694,,,BE0002204,BE0005551,BE0003546,,,BE0000984,BE0004506,BE0003481,BE0003546,,,BE0009000,BE0003546,,,BE0009865,BE0001097,BE0003790,BE0003772,,,,,BE0000985,BE0003722,,BE0002409,,BE0000854,BE0002401,,BE0003449,BE0000922,BE0008643,BE0001283,,BE0002401,BE0000215,BE0000854,BE0000393,BE0001646,BE0009698,BE0004807,BE0000030,,BE0003450,,,,,,,,,BE0001353,,,,,BE0000944,,BE0009668,BE0001097,BE0003790,BE0003772,BE0001062,,BE0004145,BE0002408,BE0003493,BE0003462,BE0003768,,BE0003997,BE0001353,,BE0000704,,BE0000981,,BE0000918,,,BE0001283,,,BE0004583,BE0002683,BE0004578,BE0010032,,BE0003433,,,,BE0002442,BE0003428,,,BE0000428,BE0001172,,,BE0004903,BE0000394,BE0003309,,BE0000854,,,BE0003517,BE0002204,BE0004907,BE0009554,BE0000428,BE0009550,BE0003546,BE0002442,BE0000574,BE0000984,BE0004506,BE0009900,BE0009903,BE0000264,BE0004903,BE0009272,BE0005551,BE0004145,BE0002408,BE0003493,BE0004146,BE0003410,BE0003409,BE0001051,BE0000147,BE0009415,BE0004655,BE0001154,BE0002394,BE0009000,BE0005076"
-// // entities = "BE0004862,,BE0000147"
-// entityList = entities.split(",")
-// runDrugBankBEProteinsOnly(entityList)
+let whats = ['transcriptomics', 'proteomics'] //proteomics / transcriptomics
+let change = 'increased' // decreased / increased
+let genes =  "TYK2"
+genes = genes.split(',')
+runDrugBankPharmaco(genes)
 
 
-// entities = ",,BE0004907,BE0004903,BE0001172,BE0000428,,,BE0005803,BE0003546,BE0009550,,BE0000985,,BE0000985,,,,,,,,,BE0000918,BE0000961,BE0001051,BE0001047,,BE0009900,BE0009903,BE0003546,BE0005076,BE0004145,BE0002408,BE0003493,,BE0000147,BE0000264,BE0009272,BE0009415,BE0009324,BE0004903,,,BE0001051,BE0002442,BE0004903,BE0008694,,,BE0002204,BE0005551,BE0003546,,,BE0000984,BE0004506,BE0003481,BE0003546,,,BE0009000,BE0003546,,,BE0009865,BE0001097,BE0003790,BE0003772,,,,,BE0000985,BE0003722,,BE0002409,,BE0000854,BE0002401,,BE0003449,BE0000922,BE0008643,BE0001283,,BE0002401,BE0000215,BE0000854,BE0000393,BE0001646,BE0009698,BE0004807,BE0000030,,BE0003450,,,,,,,,,BE0001353,,,,,BE0000944,,BE0009668,BE0001097,BE0003790,BE0003772,BE0001062,,BE0004145,BE0002408,BE0003493,BE0003462,BE0003768,,BE0003997,BE0001353,,BE0000704,,BE0000981,,BE0000918,,,BE0001283,,,BE0004583,BE0002683,BE0004578,BE0010032,,BE0003433,,,,BE0002442,BE0003428,,,BE0000428,BE0001172,,,BE0004903,BE0000394,BE0003309,,BE0000854,,,BE0003517"
-// // entities = "BE0004862,,BE0000147"
-// entityList = entities.split(",")
-// entityList = [...new Set(entityList)]
-// runDrugBankProteinsForBE(entityList)
+let cids = "564"
+cids = fs.readFileSync('ids.txt').toString()
+cids = cids.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+console.log(cids.length)
+cids = "129631922"
+cids = cids.split(',')
+runPubchemMol(cids)
 
-// let proteins = "P18848,P04839,P19838,P04114,P04839"
-// let proteinList = proteins.split(",")
-// runDrugBankProteins(proteinList)
+entities = "BE0001283"
+// entities = "BE0004862,,BE0000147"
+entityList = entities.split(",")
+runDrugBankBEProteinsOnly(entityList)
 
 
-// entities = ",,BE0004907,BE0004903,BE0001172,BE0000428,,,BE0005803,BE0003546,BE0009550,,BE0000985,,BE0000985,,,,,,,,,BE0000918,BE0000961,BE0001051,BE0001047,,BE0009900,BE0009903,BE0003546,BE0005076,BE0004145,BE0002408,BE0003493,,BE0000147,BE0000264,BE0009272,BE0009415,BE0009324,BE0004903,,,BE0001051,BE0002442,BE0004903,BE0008694,,,BE0002204,BE0005551,BE0003546,,,BE0000984,BE0004506,BE0003481,BE0003546,,,BE0009000,BE0003546,,,BE0009865,BE0001097,BE0003790,BE0003772,,,,,BE0000985,BE0003722,,BE0002409,,BE0000854,BE0002401,,BE0003449,BE0000922,BE0008643,BE0001283,,BE0002401,BE0000215,BE0000854,BE0000393,BE0001646,BE0009698,BE0004807,BE0000030,,BE0003450,,,,,,,,,BE0001353,,,,,BE0000944,,BE0009668,BE0001097,BE0003790,BE0003772,BE0001062,,BE0004145,BE0002408,BE0003493,BE0003462,BE0003768,,BE0003997,BE0001353,,BE0000704,,BE0000981,,BE0000918,,,BE0001283,,,BE0004583,BE0002683,BE0004578,BE0010032,,BE0003433,,,,BE0002442,BE0003428,,,BE0000428,BE0001172,,,BE0004903,BE0000394,BE0003309,,BE0000854,,,BE0003517"
-// // entities = ",BE0000147"
-// entityList = entities.split(",")
-// entityList = [...new Set(entityList)]
-// runDrugBankDrugsForBEandProteins(entityList)
+entities = ",,BE0004907"
+// entities = "BE0004862,,BE0000147"
+entityList = entities.split(",")
+entityList = [...new Set(entityList)]
+runDrugBankProteinsForBE(entityList)
 
-// proteins = "P18848,P04839,P19838,P04114,P04839"
-// proteins = "P04839"
-// proteinList = proteins.split(",")
-// proteinList = [...new Set(proteinList)]
-// runDrugBankDrugsForProteins(proteinList)
+let proteins = "P18848"
+let proteinList = proteins.split(",")
+runDrugBankProteins(proteinList)
 
-// drugList = fs.readFileSync('DBdownloadMols.tsv').toString()
-// drugList = drugList.split('\n').slice(0,-1).map(i => i.replace('\r',''))
-// downloadDrugBankMols(drugList)
 
-// cids = fs.readFileSync('PubChemdownloadMols.tsv').toString()
-// cids = cids.split('\n').slice(0,-1).map(i => i.replace('\r',''))
-// downloadPubchemMols(cids)
-// console.log(cids)
+entities = "BE0004907"
+// entities = ",BE0000147"
+entityList = entities.split(",")
+entityList = [...new Set(entityList)]
+runDrugBankDrugsForBEandProteins(entityList)
 
-// cids = fs.readFileSync('PubChemdownloadMols.tsv').toString()
-// cids = cids.split('\n').slice(0,-1).map(i => i.replace('\r',''))
-// downloadPubchemMolsFromPage(cids)
-// console.log(cids)
+proteins = "P04839"
+proteinList = proteins.split(",")
+proteinList = [...new Set(proteinList)]
+runDrugBankDrugsForProteins(proteinList)
 
-// let zincIds = fs.readFileSync('ZincDownloadMols.tsv').toString()
-// zincIds = zincIds.split('\n').slice(0,-1).map(i => i.replace('\r',''))
-// downloadZincMols(zincIds)
-// console.log(cids)
+drugList = fs.readFileSync('DBdownloadMols.tsv').toString()
+drugList = drugList.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+downloadDrugBankMols(drugList)
 
-// drugList = fs.readFileSync('DBdownloadMols.tsv').toString()
-// drugList = drugList.split('\n').slice(0,-1).map(i => i.replace('\r',''))
-// downloadDrugBank3Dsdfs(drugList)
+cids = fs.readFileSync('PubChemdownloadMols.tsv').toString()
+cids = cids.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+downloadPubchemMols(cids)
+console.log(cids)
 
-let uniIds = "P35354,Q04771,P35555,P21695,Q13131"
+cids = fs.readFileSync('PubChemdownloadMols.tsv').toString()
+cids = cids.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+downloadPubchemMolsFromPage(cids)
+console.log(cids)
+
+let zincIds = fs.readFileSync('ZincDownloadMols.tsv').toString()
+zincIds = zincIds.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+downloadZincMols(zincIds)
+console.log(cids)
+
+drugList = fs.readFileSync('DBdownloadMols.tsv').toString()
+drugList = drugList.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+downloadDrugBank3Dsdfs(drugList)
+
+let uniIds = "P35354"
 uniIds = uniIds.split(',')
-// uniIds = uniIds.slice(0,2)
+uniIds = uniIds.slice(0,2)
 downloadAlphaFoldPDBs(uniIds)
+
+let smilesList = fs.readFileSync('smiles.tsv').toString()
+smilesList = smilesList.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+smilesList = ["[H][C@]12C[C@H](OC)[C@H](OC)[C@@H](C(=O)OC)[C@@]1([H])C[C@@]1([H])N(CCC3=C1NC1=C3C=CC(OC)=C1)C2"]
+findCIDbySmiles(smilesList)
+
+
+let entityList = fs.readFileSync('temp.tsv').toString()
+entityList = entityList.split('\n').slice(0,-1).map(i => i.replace('\r',''))
+runDrugBankBEProteinsOnly(entityList)
